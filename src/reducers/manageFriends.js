8e2c3040c8d0fe7c7d092237d19de6ba7fb5986c
msteele96 <marcsteele2014@gmail.com>
2021-06-15 17:13:14 -0400

    commit tree a54f535594e27ef0a610edff794e4515e3199b41

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,18 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case "ADD_FRIEND":
+            return {...state,
+                friends: [
+                    ...state.friends,
+                    action.friend
+                ]
+            }
+        case "REMOVE_FRIEND":
+            const removedFriend = state.friends.filter(friend => friend.id !== action.id)
+            return {...state,
+                friends: removedFriend
+            }
+        default:
+            return state
+    }
 }
